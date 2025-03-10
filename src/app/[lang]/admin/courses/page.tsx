65251b1aa164471855082ec2199f2612db89309d
Juan Ferrer <juan.ferrer.videla@gmail.com>
2025-03-10 11:51:51 -0300

    feature(courses): add different routes

--- src/app/[lang]/admin/courses/page.tsx
+++ src/app/[lang]/admin/courses/page.tsx
@@ -1,61 +1,27 @@
-import {
-  getPresentialCourses,
-  getUsersToPresentialCourses,
-} from "@/_actions/actions";
-import { CreateCourse } from "./create-presential-course";
-import { PresentialCourse } from "./course";
-import { Suspense } from "react";
-import { GridSkeleton } from "@/components/grid-skeleton";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
 
-import { InscriptionTable } from "./inscription-table";
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query";
-
-const PresentialCourses = async () => {
-  const courses = await getPresentialCourses();
-
-  return (
-    <ul className="mb-6 sm:mb-10 md:mb-16">
-      {courses.map((course) => (
-        <li key={course.id}>
-          <PresentialCourse {...course} />
-        </li>
-      ))}
-    </ul>
-  );
-};
-
-export default async function Courses() {
-  const queryClient = new QueryClient();
-  await queryClient.prefetchQuery({
-    queryKey: ["users-to-presential-courses"],
-    queryFn: getUsersToPresentialCourses,
-  });
-
+export default function CoursesPage() {
   return (
     <>
-      <div className="flex flex-col items-center text-center">
-        <h1 className="text-4xl font-extrabold uppercase tracking-tight lg:text-5xl xl:text-6xl">
-          Courses
-        </h1>
-        <p className="mb-8 max-w-2xl scroll-m-20 text-lg font-light text-muted-foreground sm:mb-12 md:mb-16">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam facere
-        </p>
-      </div>
-      <CreateCourse />
-      <Suspense fallback={<GridSkeleton />}>
-        <PresentialCourses />
-      </Suspense>
-      <h2 className="mb-4 scroll-m-20 text-2xl font-semibold tracking-tight">
-        Cursos presenciales
-      </h2>
-
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <InscriptionTable />
-      </HydrationBoundary>
+      <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Cursos
+      </h1>
+      <nav>
+        <ul className="mt-10 flex items-center justify-center space-x-5">
+          <li>
+            <Button asChild>
+              <Link href={"/admin/courses/presential"}>Presencial</Link>
+            </Button>
+          </li>
+          <li>
+            <Button asChild>
+              <Link href={"/admin/courses/remote"}>Remoto</Link>
+            </Button>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 }
---
+++ src/app/[lang]/admin/courses/remote/page.tsx
@@ -0,0 +1,61 @@
+import {
+  getPresentialCourses,
+  getUsersToPresentialCourses,
+} from "@/_actions/actions";
+import { CreateCourse } from "../create-presential-course";
+import { PresentialCourse } from "../course";
+import { Suspense } from "react";
+import { GridSkeleton } from "@/components/grid-skeleton";
+
+import { InscriptionTable } from "../inscription-table";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+
+const PresentialCourses = async () => {
+  const courses = await getPresentialCourses();
+
+  return (
+    <ul className="mb-6 sm:mb-10 md:mb-16">
+      {courses.map((course) => (
+        <li key={course.id}>
+          <PresentialCourse {...course} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default async function Courses() {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["users-to-presential-courses"],
+    queryFn: getUsersToPresentialCourses,
+  });
+
+  return (
+    <>
+      <div className="flex flex-col items-center text-center">
+        <h1 className="text-4xl font-extrabold uppercase tracking-tight lg:text-5xl xl:text-6xl">
+          Cursos Remotos
+        </h1>
+        <p className="mb-8 max-w-2xl scroll-m-20 text-lg font-light text-muted-foreground sm:mb-12 md:mb-16">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam facere
+        </p>
+      </div>
+      <CreateCourse />
+      <Suspense fallback={<GridSkeleton />}>
+        <PresentialCourses />
+      </Suspense>
+      <h2 className="mb-4 scroll-m-20 text-2xl font-semibold tracking-tight">
+        Cursos remotos
+      </h2>
+
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <InscriptionTable />
+      </HydrationBoundary>
+    </>
+  );
+}
