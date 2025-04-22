e38d5cff5b2f0402b7a1d01c5e1a7272ca0d5dab
MatiasAlemany <matiasalemany2@gmail.com>
2025-04-22 15:24:13 +0200

    add banner & about us img

--- src/app/[lang]/(client)/events/[id]/page.tsx
+++ src/app/[lang]/(client)/events/[id]/page.tsx
@@ -29,7 +29,7 @@ export default async function EventDesc({ params }: EventDescProps) {
 
   return (
     <>
-      <h1 className="mb-8 scroll-m-20 text-center font-serif text-4xl font-extrabold tracking-tight sm:mb-12 md:mb-16 lg:text-5xl xl:text-6xl">
+      <h1 className="mb-8 scroll-m-20 text-center font-serif text-4xl tracking-tight sm:mb-12 md:mb-16 lg:text-5xl xl:text-6xl">
         {event.title}
       </h1>
       <EventCardDesc key={event.id} {...event} />
