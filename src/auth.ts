3f04dc8a7fe377569aa8ab0801b9b14d01748086
Juan Ferrer <juan.ferrer.videla@gmail.com>
2025-06-13 15:23:32 -0300

    fix: add jwt for auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,19 @@
+// auth.js (or auth.ts if you're using TS)
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
   pages: {
     signIn: "/sign-in",
+  },
+  session: {
+    strategy: "jwt", // Optional, but good to be explicit
   },
 });
